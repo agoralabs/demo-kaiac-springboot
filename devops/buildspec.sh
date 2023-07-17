738b8022ec8c0bbd32ec1dc97a011d52b0f5efc1
Joseph EKOBO KIDOU <joseph.ekobokidou@gmail.com>
2023-07-17 21:20:17 +0000

    Add Java JDK 17 Spring Boot Support for Backend

--- devops/buildspec.sh
+++ devops/buildspec.sh
@@ -6,29 +6,29 @@ echo "Build started on $THE_DATE"
 appenvsubstr(){
     p_template=$1
     p_destination=$2
-    envsubst '$TF_VAR_ENV_APP_NAME' < $p_template \
-    | envsubst '$TF_VAR_ENV_APP_ENV_NAME' \
-    | envsubst '$TF_VAR_ENV_APP_BACKEND_NAMESPACE' \
-    | envsubst '$TF_VAR_ENV_APP_BACKEND_LOCAL_SOURCE_FOLDER' \
-    | envsubst '$TF_VAR_ENV_APP_BACKEND_LOCAL_PORT' \
-    | envsubst '$TF_VAR_ENV_APP_BACKEND_URL' \
+    envsubst '$TF_VAR_ENV_APP_GL_NAME' < $p_template \
+    | envsubst '$TF_VAR_ENV_APP_GL_STAGE' \
+    | envsubst '$TF_VAR_ENV_APP_BE_NAMESPACE' \
+    | envsubst '$TF_VAR_ENV_APP_BE_LOCAL_SOURCE_FOLDER' \
+    | envsubst '$TF_VAR_ENV_APP_BE_LOCAL_PORT' \
+    | envsubst '$TF_VAR_ENV_APP_BE_URL' \
     | envsubst '$TF_VAR_ENV_APP_DATABASE_HOST' \
     | envsubst '$TF_VAR_ENV_APP_DATABASE_NAME' \
     | envsubst '$TF_VAR_ENV_APP_DATABASE_USERNAME' \
     | envsubst '$TF_VAR_ENV_APP_DATABASE_PASSWORD' \
     | envsubst '$TF_VAR_ENV_APP_DATABASE_PORT' \
-    | envsubst '$TF_VAR_ENV_APP_NAMESPACE' \
-    | envsubst '$TF_VAR_ENV_APP_AWS_ACCOUNT_ID' \
+    | envsubst '$TF_VAR_ENV_APP_GL_NAMESPACE' \
+    | envsubst '$TF_VAR_ENV_APP_GL_AWS_ACCOUNT_ID' \
     | envsubst '$TF_VAR_ENV_PHP_REPO_NAME' \
     | envsubst '$TF_VAR_ENV_PHP_REPO_TAG_APACHE' \
-    | envsubst '$TF_VAR_ENV_APP_AWS_REGION' \
-    | envsubst '$TF_VAR_ENV_APP_SCRIPT_MODE' \
-    | envsubst '$TF_VAR_ENV_APP_BACKEND_EKS_CLUSTER_NAME' \
-    | envsubst '$TF_VAR_ENV_APP_BACKEND_DOMAIN_NAME' \
-    | envsubst '$TF_VAR_ENV_APP_BACKEND_SSL_CERT_ARN' \
-    | envsubst '$TF_VAR_ENV_APP_JDK_REPO_NAME' \
-    | envsubst '$TF_VAR_ENV_APP_JDK_REPO_TAG' \
-    | envsubst '$TF_VAR_ENV_APP_AWS_REGION_ECR' > $p_destination
+    | envsubst '$TF_VAR_ENV_APP_GL_AWS_REGION' \
+    | envsubst '$TF_VAR_ENV_APP_GL_SCRIPT_MODE' \
+    | envsubst '$TF_VAR_ENV_APP_BE_EKS_CLUSTER_NAME' \
+    | envsubst '$TF_VAR_ENV_APP_BE_DOMAIN_NAME' \
+    | envsubst '$TF_VAR_ENV_APP_BE_SSL_CERT_ARN' \
+    | envsubst '$TF_VAR_ENV_APP_GL_REPO_JDK_NAME' \
+    | envsubst '$TF_VAR_ENV_APP_GL_REPO_JDK_TAG' \
+    | envsubst '$TF_VAR_ENV_APP_GL_AWS_REGION_ECR' > $p_destination
 }
 
 appenvsubstr devops/appspec.yml.template appspec.yml
@@ -39,13 +39,13 @@ chmod 777 devops/appspec.sh
 chmod +x ./mvnw
 ./mvnw clean install -DskipTests
 
-if [ "$TF_VAR_ENV_APP_SCRIPT_MODE" == "CLOUDOCKER" ] 
+if [ "$TF_VAR_ENV_APP_GL_SCRIPT_MODE" == "CLOUDOCKER" ] 
 then
 
     appenvsubstr devops/Dockerfile.template Dockerfile
     appenvsubstr devops/docker-compose.yml.template docker-compose.yml
 
-elif [ "$TF_VAR_ENV_APP_SCRIPT_MODE" == "CLOUDEKS" ] 
+elif [ "$TF_VAR_ENV_APP_GL_SCRIPT_MODE" == "CLOUDEKS" ] 
 then
     echo "Generating Dockerfile..."
     appenvsubstr devops/Dockerfile.template Dockerfile
@@ -57,31 +57,31 @@ then
     appenvsubstr devops/app-service.yaml.template app-service.yaml
 
     echo "Login into ecr..."
-    aws ecr get-login-password --region $TF_VAR_ENV_APP_AWS_REGION_ECR | docker login --username AWS --password-stdin $TF_VAR_ENV_APP_AWS_ACCOUNT_ID.dkr.ecr.$TF_VAR_ENV_APP_AWS_REGION_ECR.amazonaws.com
+    aws ecr get-login-password --region $TF_VAR_ENV_APP_GL_AWS_REGION_ECR | docker login --username AWS --password-stdin $TF_VAR_ENV_APP_GL_AWS_ACCOUNT_ID.dkr.ecr.$TF_VAR_ENV_APP_GL_AWS_REGION_ECR.amazonaws.com
 
     echo "Building the Docker image..."
-    docker build -t $TF_VAR_ENV_APP_NAME:${TF_VAR_ENV_APP_BACKEND_NAMESPACE}_${TF_VAR_ENV_APP_NAME} .
+    docker build -t $TF_VAR_ENV_APP_GL_NAME:${TF_VAR_ENV_APP_BE_NAMESPACE}_${TF_VAR_ENV_APP_GL_NAME} .
 
-    echo "Create $TF_VAR_ENV_APP_NAME repository..."
-    aws ecr describe-repositories --repository-names $TF_VAR_ENV_APP_NAME || aws ecr create-repository --repository-name $TF_VAR_ENV_APP_NAME
+    echo "Create $TF_VAR_ENV_APP_GL_NAME repository..."
+    aws ecr describe-repositories --repository-names $TF_VAR_ENV_APP_GL_NAME || aws ecr create-repository --repository-name $TF_VAR_ENV_APP_GL_NAME
 
     echo "Tag your image with the Amazon ECR registry..."
-    docker tag $TF_VAR_ENV_APP_NAME:${TF_VAR_ENV_APP_BACKEND_NAMESPACE}_${TF_VAR_ENV_APP_NAME} $TF_VAR_ENV_APP_AWS_ACCOUNT_ID.dkr.ecr.$TF_VAR_ENV_APP_AWS_REGION_ECR.amazonaws.com/$TF_VAR_ENV_APP_NAME:$TF_VAR_ENV_APP_BACKEND_NAMESPACE'_'$TF_VAR_ENV_APP_NAME
+    docker tag $TF_VAR_ENV_APP_GL_NAME:${TF_VAR_ENV_APP_BE_NAMESPACE}_${TF_VAR_ENV_APP_GL_NAME} $TF_VAR_ENV_APP_GL_AWS_ACCOUNT_ID.dkr.ecr.$TF_VAR_ENV_APP_GL_AWS_REGION_ECR.amazonaws.com/$TF_VAR_ENV_APP_GL_NAME:$TF_VAR_ENV_APP_BE_NAMESPACE'_'$TF_VAR_ENV_APP_GL_NAME
 
     echo "Push the image to ecr..."
-    docker push $TF_VAR_ENV_APP_AWS_ACCOUNT_ID.dkr.ecr.$TF_VAR_ENV_APP_AWS_REGION_ECR.amazonaws.com/$TF_VAR_ENV_APP_NAME:$TF_VAR_ENV_APP_BACKEND_NAMESPACE'_'$TF_VAR_ENV_APP_NAME
+    docker push $TF_VAR_ENV_APP_GL_AWS_ACCOUNT_ID.dkr.ecr.$TF_VAR_ENV_APP_GL_AWS_REGION_ECR.amazonaws.com/$TF_VAR_ENV_APP_GL_NAME:$TF_VAR_ENV_APP_BE_NAMESPACE'_'$TF_VAR_ENV_APP_GL_NAME
 
     echo "Updating kubeconfig..."
-    aws eks update-kubeconfig --region $TF_VAR_ENV_APP_AWS_REGION --name $TF_VAR_ENV_APP_BACKEND_EKS_CLUSTER_NAME
+    aws eks update-kubeconfig --region $TF_VAR_ENV_APP_GL_AWS_REGION --name $TF_VAR_ENV_APP_BE_EKS_CLUSTER_NAME
     
     cat app-kubernetes.yaml
     cat app-service.yaml
 
     echo "Trying kubectl apply -f app-kubernetes.yaml..."
-    kubectl apply -f app-kubernetes.yaml -n ${TF_VAR_ENV_APP_BACKEND_KUBERNETES_NAMESPACE}
+    kubectl apply -f app-kubernetes.yaml -n ${TF_VAR_ENV_APP_BE_KUBERNETES_NAMESPACE}
     
     echo "Trying kubectl apply -f app-service.yaml..."
-    kubectl apply -f app-service.yaml -n ${TF_VAR_ENV_APP_BACKEND_KUBERNETES_NAMESPACE}
+    kubectl apply -f app-service.yaml -n ${TF_VAR_ENV_APP_BE_KUBERNETES_NAMESPACE}
 
 fi
 
