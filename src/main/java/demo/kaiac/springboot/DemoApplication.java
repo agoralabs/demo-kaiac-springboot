52e9bdf19d7f4a39ae8c98a525db19b1a27c121d
Joseph EKOBO KIDOU <joseph.ekobokidou@gmail.com>
2023-07-21 22:44:06 +0000

    Add Java JDK 17 Spring Boot Support for Backend

--- src/main/java/demo/kaiac/springboot/DemoApplication.java
+++ src/main/java/demo/kaiac/springboot/DemoApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "demo.kaiac.springboot.api")
-@EnableJpaRepositories("demo.kaiac.springboot.api.repository")
+@SpringBootApplication
 @EntityScan("demo.kaiac.springboot.api.pojo")
 public class DemoApplication {
 
