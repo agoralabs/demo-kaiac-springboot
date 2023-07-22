2cd31c72f28ddd6516bf9029b004c58c3537e390
Joseph EKOBO KIDOU <joseph.ekobokidou@gmail.com>
2023-07-22 20:02:38 +0000

    Add Java JDK 17 Spring Boot Support for Backend

--- src/main/java/demo/kaiac/springboot/api/ApiDemoController.java
+++ src/main/java/demo/kaiac/springboot/api/ApiDemoController.java
@@ -44,7 +44,7 @@ public class ApiDemoController {
         projectList.add(new Project(1,"IMDB","1st June 2023","overdue", "Lorem ipsum dolor sit amet consectetur adipisicing elit.", responsible1));
         Member responsible2 = new Member(2,"Joseph", "Future", "/team-member-2.jpg", "joseph.future@agoralabs.org");
         projectList.add(new Project(2,"Frontend","20th June 2023","ongoing", "Lorem ipsum dolor sit amet consectetur adipisicing elit.", responsible2));
-        Member responsible3 = new Member(3,"Charles", "Pika", "/team-member-3.jpg", "dorine.michou@agoralabs.org");
+        Member responsible3 = new Member(3,"Charles", "Pika", "/team-member-3.jpg", "charles.pika@agoralabs.org");
         projectList.add(new Project(3,"Backend","10th June 2023","completed", "Lorem ipsum dolor sit amet consectetur adipisicing elit.", responsible3));
         Member responsible4 = new Member(4,"Wesley", "Weezy", "/team-member-4.jpg", "wesley.weezy@agoralabs.org");
         projectList.add(new Project(4,"Database","1st July 2023","ongoing", "Lorem ipsum dolor sit amet consectetur adipisicing elit.", responsible4));
@@ -54,7 +54,7 @@ public class ApiDemoController {
     @PostMapping(value = "/project")
     public Project createProduct() {
         log.debug("Inside of createProduct() method ");
-        Member responsible5 = new Member(5,"Wesley", "Weezy", "/team-member-5.jpg");
+        Member responsible5 = new Member(5,"Dorine", "Michou", "/team-member-5.jpg", "dorine.michou@agoralabs.org");
         return new Project(5,"New Project","1st July 2023","ongoing", "Lorem ipsum dolor sit amet consectetur adipisicing elit.", responsible5);
     }
 
