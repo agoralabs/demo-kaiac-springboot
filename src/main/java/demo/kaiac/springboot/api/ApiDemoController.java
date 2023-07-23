27ac2e4335c4063d23b41b2cf364ecfcab43ed4a
Joseph EKOBO KIDOU <joseph.ekobokidou@gmail.com>
2023-07-23 00:54:44 +0000

    Add Java JDK 17 Spring Boot Support for Backend

--- src/main/java/demo/kaiac/springboot/api/ApiDemoController.java
+++ src/main/java/demo/kaiac/springboot/api/ApiDemoController.java
@@ -43,18 +43,28 @@ public class ApiDemoController {
 
 
     @GetMapping("/projects")
-    public List<Project> getProjects() {
+    public @ResponseBody Iterable<Project> getProjects() {
         log.debug("Inside of getProjects() method ");
-        List<Project> projectList = new ArrayList<>();
-        Member responsible1 = new Member(1,"Cathy", "Coulaly", "/team-member-1.jpg", "cathy.coulaly@agoralabs.org");
-        projectList.add(new Project(1,"SB-IMDB","1st June 2023","overdue", "Lorem ipsum dolor sit amet consectetur adipisicing elit.", responsible1));
-        Member responsible2 = new Member(2,"Joseph", "Future", "/team-member-2.jpg", "joseph.future@agoralabs.org");
-        projectList.add(new Project(2,"SB-Frontend","20th June 2023","ongoing", "Lorem ipsum dolor sit amet consectetur adipisicing elit.", responsible2));
-        Member responsible3 = new Member(3,"Charles", "Pika", "/team-member-3.jpg", "charles.pika@agoralabs.org");
-        projectList.add(new Project(3,"SB-Backend","10th June 2023","completed", "Lorem ipsum dolor sit amet consectetur adipisicing elit.", responsible3));
-        Member responsible4 = new Member(4,"Wesley", "Weezy", "/team-member-4.jpg", "wesley.weezy@agoralabs.org");
-        projectList.add(new Project(4,"SB-Database","1st July 2023","ongoing", "Lorem ipsum dolor sit amet consectetur adipisicing elit.", responsible4));
-        return projectList;
+
+        try {
+            //  Block of code to try
+            // This returns a JSON or XML with the projects
+            return projectRepository.findAll();
+        }
+        catch(Exception e) {
+            //  Block of code to handle errors
+            List<Project> projectList = new ArrayList<>();
+            Member responsible1 = new Member(1,"Cathy", "Coulaly", "/team-member-1.jpg", "cathy.coulaly@agoralabs.org");
+            projectList.add(new Project(1,"SB-IMDB","1st June 2023","overdue", "Lorem ipsum dolor sit amet consectetur adipisicing elit.", responsible1));
+            Member responsible2 = new Member(2,"Joseph", "Future", "/team-member-2.jpg", "joseph.future@agoralabs.org");
+            projectList.add(new Project(2,"SB-Frontend","20th June 2023","ongoing", "Lorem ipsum dolor sit amet consectetur adipisicing elit.", responsible2));
+            Member responsible3 = new Member(3,"Charles", "Pika", "/team-member-3.jpg", "charles.pika@agoralabs.org");
+            projectList.add(new Project(3,"SB-Backend","10th June 2023","completed", "Lorem ipsum dolor sit amet consectetur adipisicing elit.", responsible3));
+            Member responsible4 = new Member(4,"Wesley", "Weezy", "/team-member-4.jpg", "wesley.weezy@agoralabs.org");
+            projectList.add(new Project(4,"SB-Database","1st July 2023","ongoing", "Lorem ipsum dolor sit amet consectetur adipisicing elit.", responsible4));
+            return projectList;
+        }
+
     }
 
     @PostMapping(value = "/project")
@@ -64,22 +74,33 @@ public class ApiDemoController {
         return new Project(5,"SB-Project5","1st July 2023","ongoing", "Lorem ipsum dolor sit amet consectetur adipisicing elit.", responsible5);
     }
 
-    @GetMapping(path="/allprojects")
-    public @ResponseBody Iterable<Project> getAllProjects() {
-        // This returns a JSON or XML with the projects
-        return projectRepository.findAll();
-    }
-
     @GetMapping(path="/users")
     public @ResponseBody Iterable<Member> getAllUsers() {
-        // This returns a JSON or XML with the users
-        return memberRepository.findAll();
+        log.debug("Inside of getAllUsers() method ");
+        try {
+            // This returns a JSON or XML with the users
+            return memberRepository.findAll();
+        }
+        catch(Exception e) {
+            //  Block of code to handle errors
+            return null;
+        }
+
     }
 
     @GetMapping(path="/jobroles")
     public @ResponseBody Iterable<JobRole> getAllJobRoles() {
-        // This returns a JSON or XML with the job_roles
-        return jobRoleRepository.findAll();
+        log.debug("Inside of getAllJobRoles() method ");
+        try {
+            //  Block of code to try
+            // This returns a JSON or XML with the job_roles
+            return jobRoleRepository.findAll();
+        }
+        catch(Exception e) {
+            //  Block of code to handle errors
+            return null;
+        }
+
     }
 
 }
