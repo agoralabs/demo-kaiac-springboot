616415637d23216969bb9135f5d2308f158b4d67
Joseph EKOBO KIDOU <joseph.ekobokidou@gmail.com>
2023-07-23 01:22:20 +0000

    Add Java JDK 17 Spring Boot Support for Backend

--- src/main/java/demo/kaiac/springboot/api/ApiDemoController.java
+++ src/main/java/demo/kaiac/springboot/api/ApiDemoController.java
@@ -3,8 +3,7 @@ package demo.kaiac.springboot.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
